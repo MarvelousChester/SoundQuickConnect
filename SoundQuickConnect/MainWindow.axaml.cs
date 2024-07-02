fbe09d23a67cc57b116964e0014158d406a5568e
Karandeep Sandhu <90730377+MarvelousChester@users.noreply.github.com>
2024-07-02 15:16:43 -0400

    Initialize project

---
+++ SoundQuickConnect/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace SoundQuickConnect;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+    }
+    
+
+}
